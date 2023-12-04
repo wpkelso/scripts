b69be726039d8b67a2cc012482e45f563a8a2c03
wpkelso <wkelso02@gmail.com>
2023-12-04 09:37:05 -0500

    added script to update/backup configs to/from git

---
+++ update-configs.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# This script assumes that the git/backup directory is ~/Documents/configs
+# and follows a similar directory hierarchy as wpkelso/configs
+
+VERSION="0.1"
+OP_FLAG="BACKUP"
+
+while getopts 'vsb?' OPTION; do
+    case "$OPTION" in
+        v)
+            echo "System/Application Config Update script version $VERSION"
+            exit 1
+            ;;
+        s)
+            echo "Using operation: SET"
+            OP_FLAG="SET"
+            ;;
+        b)
+            echo "Using operation: BACKUP"
+            OP_FLAG="BACKUP"
+            ;;
+        ?)
+            echo "Usage: $(basename $0)" >&2
+            ;;
+    esac
+done
+
+if [ "SET" == $OP_FLAG ]; then
+    echo "Beginning operation: SET..."
+
+    echo "[Alacritty] Copying from git dir to local"
+    cp ~/Documents/configs/alacritty/alacritty.yml ~/.config/alacritty/
+
+    echo "[NVim] Copying from git dir to local"
+    cp ~/Documents/configs/nvim/init.lua ~/.config/nvim/
+
+    echo "[TMux] Copying from git dir to local"
+    cp ~/Documents/configs/tmux/.tmux.conf ~/
+
+    echo "[zsh] Copying from git dir to local"
+    echo "!!! Note: oh-my-zsh should be installed for this config to work"
+    cp ~/Documents/configs/zsh/.zshrc ~/
+
+    echo "Set all configs in repo, operation finished"
+
+else if [ "BACKUP" == $OP_FLAG ]; then
+    echo "Beginning operation: BACKUP..."
+
+    echo "[Alacritty] Copying from local dir to git"
+    cp ~/.config/alacritty/alacritty.yml ~/Documents/configs/alacritty/
+
+    echo "[NVim] Copying from git dir to local"
+    cp ~/.config/nvim/init.lua ~/Documents/configs/nvim/init.lua
+
+    echo "[TMux] Copying from git dir to local"
+    cp ~/.tmux.conf ~/Documents/configs/tmux/
+
+    echo "[zsh] Copying from git dir to local"
+    cp ~/.zshrc ~/Documents/configs/zsh/
+
+    echo "Set all configs in repo, operation finished"
+
+
+else
+    echo "Flag error in script, exiting..."
+    exit 1
+fi
